f2766c9f840772f60afc8a0ceb793e6353745957
Kaaner <kaaner4mir@gmail.com>
2025-08-31 02:42:07 +0300

    first commit

---
+++ AutomatedTellerMachine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+
+    }
+
+
+    private static void SpinnerAnimation()
+    {
+        char[] chars = { '-', '\\', '|', '/' };
+
+        short loopCounter = 0;
+        const short loopDuration = 50;
+
+        Console.CursorVisible = false;
+
+        while (loopCounter < 5)
+        {
+            foreach (var item in chars)
+            {
+                Console.Write($"\r" + $"{item}");
+                Thread.Sleep(loopDuration);
+            }
+            loopCounter++;
+        }
+        Console.CursorVisible = true;
+        Console.Clear();
+    }
+}
