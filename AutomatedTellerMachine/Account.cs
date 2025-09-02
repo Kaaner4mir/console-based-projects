10498dc1a6e14c089a23c8f0df7badf5f10c36d4
Kaaner <kaaner4mir@gmail.com>
2025-09-03 00:02:34 +0300

    feat : add new properties

--- AutomatedTellerMachine/Account.cs
+++ AutomatedTellerMachine/Account.cs
@@ -6,4 +6,18 @@
     public string? Currency { get; set; }
     public DateTime CreatedDate { get; set; }
     public decimal? Balance { get; set; }
+    public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+}
+
+class CreditCard : Account
+{
+    public int AccountId { get; set; }
+    public string? CardName { get; set; }
+    public string? Branch { get; set; }
+    public string? Currency { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public decimal? Balance { get; set; }
+    public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
 }
